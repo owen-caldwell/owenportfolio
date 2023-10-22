56ac28f14af1ceb553abedd4680719adeeb4e0ab
owen-caldwell <caldwellom@gmail.com>
2023-10-22 18:49:23 -0400

    finished?

---
+++ switch.js
@@ -0,0 +1,37 @@
+const bio = document.querySelector(".bio");
+const work = document.querySelector(".work");
+const infobtn = document.querySelector("#infobtn");
+const workbtn = document.querySelector("#workbtn");
+const extworkbtn = document.querySelector("#extworkbtn");
+bio.style.display = "block";
+infobtn.style.textDecoration = "underline";
+work.style.display = "none";
+workbtn.style.textDecoration = "none";
+extworkbtn.style.textDecoration = "underline";
+extworkbtn.addEventListener("click", () => {
+    work.style.display = "block";
+    workbtn.style.textDecoration = "underline";
+    bio.style.display = "none";
+    infobtn.style.textDecoration = "none";
+})
+infobtn.addEventListener("click", () => {
+    work.style.display = "none";
+    workbtn.style.textDecoration = "none";
+    bio.style.display = "block";
+    infobtn.style.textDecoration = "underline";
+})
+workbtn.addEventListener("click", () => {
+    work.style.display = "block";
+    workbtn.style.textDecoration = "underline";
+    bio.style.display = "none";
+    infobtn.style.textDecoration = "none";
+})
+workbtn.addEventListener("mouseover", () => {
+    workbtn.style.textDecoration = "underline"
+})
+infobtn.addEventListener("mouseover", () => {
+    infobtn.style.textDecoration = "underline"
+})
+extworkbtn.addEventListener("mouseover", () => {
+    extworkbtn.style.cursor = "pointer";
+})
